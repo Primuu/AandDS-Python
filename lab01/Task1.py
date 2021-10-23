d91daf7a3bd3860531f68fbbe9600eed9be6cbdf
Adam Trentowski <trentowski.adam@gmail.com>
2021-10-23 20:57:18 +0200

    Lab01/02/03 - LL, Stack, Queue

--- lab01/Task1.py
+++ lab01/Task1.py
@@ -3,31 +3,62 @@ from typing import Any
 
 class Node:
     def __init__(self, value: Any) -> None:
-        # ( , next: Node)
         self.value = value
         self.next = None
 
 
 class LinkedList:
     def __init__(self) -> None:
-        # ( , head: Node, tail: Node)
         self.head = None
         self.tail = None
 
+    def __len__(self) -> int:
+        length = 0
+        el_iterator = self.head
+        if el_iterator is None:
+            return length
+        length = 1
+        while el_iterator.next:
+            length += 1
+            el_iterator = el_iterator.next
+        return length
+
+    def __str__(self) -> str:
+        print_iterator = self.head
+        print_value = ""
+        while print_iterator is not None:
+            print_value = print_value + str(print_iterator.value)
+            print_iterator = print_iterator.next
+            if print_iterator is not None:
+                print_value = print_value + " -> "
+        return print_value
+
     def push(self, new_value: Any) -> None:
         new_node = Node(new_value)
+        if self.head is None:
+            self.tail = new_node
         new_node.next = self.head
         self.head = new_node
+
+# First version (by iterating)
+    # def append(self, new_value: Any) -> None:
+    #     new_node = Node(new_value)
+    #     if self.head is None:
+    #         self.head = new_node
+    #         return
+    #     last_empty = self.head
+    #     while last_empty.next:
+    #         last_empty = last_empty.next
+    #     last_empty.next = new_node
 
     def append(self, new_value: Any) -> None:
         new_node = Node(new_value)
         if self.head is None:
             self.head = new_node
+            self.tail = new_node
             return
-        last_empty = self.head
-        while last_empty.next:
-            last_empty = last_empty.next
-        last_empty.next = new_node
+        self.tail.next = new_node
+        self.tail = new_node
 
     def node(self, at: int) -> Node:
         returned_node = self.head
@@ -44,12 +75,20 @@ class LinkedList:
 
     def insert(self, new_value: Any, after: Node) -> None:
         new_node = Node(new_value)
+        if after == self.tail:
+            after.next = new_node
+            self.tail = new_node
         new_node.next = after.next
         after.next = new_node
 
     def pop(self) -> Any:
         if self.head is None:
             return None
+        if self.head.next is None:
+            deleted_node_value = self.head.value
+            self.head = self.head.next
+            self.tail = None
+            return deleted_node_value
         deleted_node_value = self.head.value
         self.head = self.head.next
         return deleted_node_value
@@ -59,6 +98,7 @@ class LinkedList:
         while last_empty.next.next:
             last_empty = last_empty.next
         deleted_node_value = last_empty.next.value
+        self.tail = last_empty
         last_empty.next = None
         return deleted_node_value
 
@@ -67,40 +107,73 @@ class LinkedList:
             return None
         if after.next is None:
             return None
+        if after.next == self.tail:
+            self.tail = after
+            after.next = None
+            return None
         after.next = after.next.next
 
-    def __str__(self) -> str:
-        print_iterator = self.head
-        print_value = ""
-        while print_iterator is not None:
-            print_value = print_value + str(print_iterator.value)
-            print_iterator = print_iterator.next
-            if print_iterator is not None:
-                print_value = print_value + " -> "
-        return print_value
+
+class Stack:
+    def __init__(self) -> None:
+        self._storage = LinkedList()
 
     def __len__(self) -> int:
-        length = 0
-        el_iterator = self.head
-        if el_iterator is None:
-            return length
-        length = 1
-        while el_iterator.next:
-            length += 1
-            el_iterator = el_iterator.next
-        return length
+        return len(self._storage)
+
+    def __str__(self) -> str:
+        print_value = ""
+        for x in range(0, len(self._storage)):
+            print_value += str(self._storage.node(x).value) + "\n"
+        return print_value
+
+    def push(self, element: Any) -> None:
+        self._storage.push(element)
+
+    def pop(self) -> Any:
+        return self._storage.pop()
 
 
+class Queue:
+    def __init__(self) -> None:
+        self._storage = LinkedList()
+
+    def __len__(self) -> int:
+        return len(self._storage)
+
+    def __str__(self) -> str:
+        print_value = ""
+        for x in range(0, len(self._storage)):
+            print_value += str(self._storage.node(x).value)
+            if x + 1 < len(self._storage):
+                print_value += ", "
+        return print_value
+
+    def peek(self) -> Any:
+        return self._storage.head.value
+
+    def enqueue(self, element: Any) -> None:
+        self._storage.append(element)
+
+    def dequeue(self) -> Any:
+        return self._storage.pop()
+
+
+# -----------------------------------------------------------------------
 # First assert
 list1 = LinkedList()
 assert list1.head is None
+assert list1.tail is None
 
 
 # Second assert
 list1.push(1)
+assert list1.tail is not None
+# print(list1.tail.value)
 list1.push(0)
 assert str(list1) == '0 -> 1'
 print("\nSecond assert (push): \n" + str(list1))
+# print(list1.tail.value)
 
 
 # Third assert
@@ -108,6 +181,7 @@ list1.append(9)
 list1.append(10)
 assert str(list1) == '0 -> 1 -> 9 -> 10'
 print("\nThird assert (append): \n" + str(list1))
+# print(list1.tail.value)
 
 
 # Fourth  assert
@@ -149,4 +223,43 @@ print("\n" + str(list2) + "\n Length(1): " + str(len(list2)))
 list2.append(2)
 list2.append(3)
 list2.append(4)
-print("\n" + str(list2) + "\n Length(4): " + str(len(list2)))
+print("\n" + str(list2) + "\n Length(4): " + str(len(list2)) + "\n")
+
+
+# -----------------------------------------------------------------------
+# Stack asserts (1st)
+stack = Stack()
+assert len(stack) == 0
+
+# Stack asserts (2nd)
+stack.push(3)
+stack.push(10)
+stack.push(1)
+assert len(stack) == 3
+print("\n\nStack asserts:\n" + str(stack))
+
+# Stack asserts (3rd)
+top_value = stack.pop()
+assert top_value == 1
+assert len(stack) == 2
+print(stack)
+
+
+# -----------------------------------------------------------------------
+# Queue asserts (1st)
+queue = Queue()
+assert len(queue) == 0
+
+queue.enqueue('klient1')
+queue.enqueue('klient2')
+queue.enqueue('klient3')
+assert str(queue) == 'klient1, klient2, klient3'
+print("\n\nQueue asserts:\n" + str(queue))
+# print(queue.peek())
+
+# Queue asserts (2nd)
+client_first = queue.dequeue()
+assert client_first == 'klient1'
+assert str(queue) == 'klient2, klient3'
+assert len(queue) == 2
+print(queue)
